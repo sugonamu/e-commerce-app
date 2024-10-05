4356282088b1a8e4a8d3438465d998349ef51ca6
sugonamu <je0jwff@gmail.com>
2024-10-05 19:12:52 +0700

    fixed product display, edit and delete

--- main1/urls.py
+++ main1/urls.py
@@ -6,6 +6,7 @@ app_name = 'main'
 urlpatterns = [
     path('', views.info_view, name='home'),
     path('info/', views.info_view, name='info_view'),
+    path('get-user-products/', views.info_view, name='get_user_products'), 
     path('add-product/', views.add_product, name='add_product'),
     path('json/', views.products_json, name='products_json'),
     path('json/<int:product_id>/', views.product_json_by_id, name='product_json_by_id'),
@@ -14,9 +15,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('edit-product/<uuid:product_id>/', views.edit_product, name='edit_product'),
-    path('delete/<uuid:product_id>', views.delete_product, name='delete_product'),
+    path('edit/<uuid:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<uuid:product_id>/', views.delete_product, name='delete_product'),
     path('error/', views.error, name='error'),
     path('create-ajax/', views.create_product_ajax, name='create_product_ajax'),
-
 ]
--- main1/views.py
+++ main1/views.py
@@ -1,28 +1,28 @@
 import datetime
-from django.shortcuts import render,redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .models import Product
 from .forms import ProductForm
-from django.http import HttpResponse,HttpResponseRedirect, JsonResponse
-from django.core import serializers
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
+from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/login')
 def info_view(request):
     products = Product.objects.filter(user=request.user)
     
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':  # Check if the request is AJAX
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         products_data = [
             {
                 'pk': product.pk,
                 'name': product.name,
                 'description': product.description,
                 'price': product.price,
-                'image_url': product.image_url,
+                'image_url': product.image.url if product.image else None,
             }
             for product in products
         ]
@@ -34,7 +34,7 @@ def info_view(request):
         'your_name': 'Will',
         'your_class': 'KKI',
         'products': products,
-        'last_login': request.COOKIES['last_login']
+        'last_login': request.COOKIES.get('last_login', 'No login record found')
     }
     return render(request, 'main1.html', context)
 
@@ -45,7 +45,7 @@ def add_product(request):
             product = form.save(commit=False)
             product.user = request.user  # Assign the logged-in user
             product.save()
-            return redirect('main:info_view')  # Redirect to a success page or product list
+            return redirect('main:info_view')
     else:
         form = ProductForm()
     
@@ -57,30 +57,24 @@ def products_json(request):
     return JsonResponse(data, safe=False)
 
 def product_json_by_id(request, product_id):
-    try:
-        product = Product.objects.get(id=product_id)
-        data = {
-            'name': product.name,
-            'price': product.price,
-            'description': product.description,
-            'image': product.image.url if product.image else None  # Handle image URL
-        }
-        return JsonResponse(data)
-    except Product.DoesNotExist:
-        return JsonResponse({'error': 'Product not found'}, status=404)
+    product = get_object_or_404(Product, id=product_id)
+    data = {
+        'name': product.name,
+        'price': product.price,
+        'description': product.description,
+        'image': product.image.url if product.image else None
+    }
+    return JsonResponse(data)
 
 def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_xml_by_id(request, product_id):
-    try:
-        product = Product.objects.get(id=product_id)
-        data = serializers.serialize("xml", [product])
-        return HttpResponse(data, content_type="application/xml")
-    except Product.DoesNotExist:
-        return HttpResponse('<error>Product not found</error>', content_type="application/xml")
-    
+    product = get_object_or_404(Product, id=product_id)
+    data = serializers.serialize("xml", [product])
+    return HttpResponse(data, content_type="application/xml")
+
 def register(request):
     form = UserCreationForm()
 
@@ -90,30 +84,24 @@ def register(request):
             form.save()
             messages.success(request, 'Your account has been successfully created!')
             return redirect('main:login')
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+        if form.is_valid():
             user = form.get_user()
-            
             login(request, user)
             response = HttpResponseRedirect(reverse("main:info_view"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
-            if user is not None:
-                login(request, user)
-                response = HttpResponseRedirect(reverse("main:info_view"))
-                response.set_cookie('last_login', str(datetime.datetime.now()))
-                return response
             return response
 
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
@@ -122,9 +110,8 @@ def logout_user(request):
     return response
 
 def edit_product(request, product_id):
-    product = Product.objects.get(pk=product_id)
-
-    form = ProductForm(request.POST or None, instance=product)
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(request.POST or None, request.FILES or None, instance=product)
 
     if form.is_valid() and request.method == "POST":
         form.save()
@@ -134,22 +121,54 @@ def edit_product(request, product_id):
     return render(request, "editproduct.html", context)
 
 def delete_product(request, product_id):
-    product = Product.objects.get(pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
     product.delete()
     return HttpResponseRedirect(reverse('main:info_view'))
 
 def error(request):
     return render(request, 'error.html') 
 
-@csrf_exempt
+@csrf_exempt  # Remove if using CSRF tokens in frontend
+@require_POST
 def create_product_ajax(request):
-    if request.method == "POST":
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save(commit=False)
-            product.user = request.user
-            product.save()
-            return JsonResponse({'success': True})
-        else:
-            return JsonResponse({'success': False, 'error': 'Invalid form data'}, status=400)
-    return JsonResponse({'success': False, 'error': 'Invalid request method'}, status=405)
+    name = strip_tags(request.POST.get("name"))
+    description = strip_tags(request.POST.get("description"))
+    price = request.POST.get("price")
+    image_url = request.POST.get("image")
+    user = request.user
+    
+    if not name or not description or not price:
+        return JsonResponse({"error": "Invalid data"}, status=400)
+
+    try:
+        price = float(price)
+    except ValueError:
+        return JsonResponse({"error": "Price must be a valid number"}, status=400)
+
+    new_product = Product(
+        name=name,
+        description=description,
+        price=price,
+        image=image_url,  
+        user=user
+    )
+    new_product.save()
+
+    return JsonResponse({"success": True}, status=201)
+
+def get_user_products(request):
+    if request.user.is_authenticated:
+        products = Product.objects.filter(user=request.user)
+        product_data = [
+            {
+                'id': str(product.id),
+                'name': product.name,
+                'description': product.description,
+                'price': product.price,
+                'image_url': product.image.url if product.image else None,
+            }
+            for product in products
+        ]
+        return JsonResponse({'products': product_data})
+    else:
+        return JsonResponse({'products': []})
